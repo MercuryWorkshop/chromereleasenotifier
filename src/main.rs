3a6999d4b3e20aabac2d35f35303da85e34fa125
r58Playz <r58playz@gmail.com>
2023-03-03 14:30:19 -0800

    use notify_rust now

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,19 @@ fn main() {
     let mut reader = Reader::from_str(&body);
     reader.trim_text(true);
 
-    let mut toprint: Vec<String> = Vec::new();
+    let mut contents: Vec<String> = Vec::new();
 
     loop {
         match reader.read_event() {
             Ok(Event::Start(e)) => {
-                if e.name().as_ref() == b"title" {
-                    let txt = reader
-                        .read_text(e.name())
-                        .expect("Cannot decode title value").to_string();
-                    if txt.contains("Stable Channel Update for ChromeOS/ChromeOS Flex") {
-                        toprint.push(txt);
-                    }
-                } else if e.name().as_ref() == b"content" {
+                if e.name().as_ref() == b"content" {
                     let txt = reader
                         .read_text(e.name())
                         .expect("Cannot decode content value");
                     let unescaped = quick_xml::escape::unescape(&txt).expect("Cannot unescape content").to_string();
                     let textified = nanohtml2text::html2text(&unescaped).to_string().replace("\r\n", "\n");
                     if txt.contains("Stable channel") && txt.contains("ChromeOS")  {
-                        toprint.push(textified);
-                        toprint.push("============".to_string());
+                        contents.push(textified);
                     }
                 }
             }
@@ -44,8 +36,30 @@ fn main() {
         }
     }
 
-    let slice = &toprint;
-    for i in slice {
-        println!("{}", i);
+    let contentslice = &contents;
+    for content in contentslice {
+        let splittedcontent = content.split("\n");
+        let splittedvec: Vec<&str> = splittedcontent.collect();
+        let mut filteredvec: Vec<String> = Vec::new();
+        let mut isaftersecurityfixes = false;
+        for line in splittedvec {
+            if line == "" {
+                // skip all empty lines
+            } else if line.contains("is being updated to") {
+                filteredvec.push(line.to_string());
+            } else if line.contains("Security Fixes and Rewards") {
+                filteredvec.push("".to_string());
+                filteredvec.push(line.to_string());
+                isaftersecurityfixes = true;
+            } else if isaftersecurityfixes && !(line.contains("Access to bug details and links") || line.contains("We would also like to thank")) {
+                filteredvec.push(line.to_string());
+            }
+        }
+        let filteredcontent = filteredvec.join("\n");
+        notify_rust::Notification::new()
+                            .summary("Chrome Releases Notifier")
+                            .body(&filteredcontent)
+                            .show()
+                            .expect("Failed to show notification");
     }
 }
