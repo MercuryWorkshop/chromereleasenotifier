9b5dc8191d7b8a66b86b75d69b165b4d392e7481
r58Playz <r58playz@gmail.com>
2023-07-30 08:51:06 -0700

    diff system

--- chromereleasesnotifierbot.py
+++ chromereleasesnotifierbot.py
@@ -10,8 +10,8 @@ TOKEN="kde plasma token"
 people = ["CoolElectronics", "Rafflesia", "r58Playz", "kotlin", "Astral", "Catakang", "avd3"]
 channelId=1066136075703177337
 if not (os.path.exists(f"{SCRDIR}/prevoutput.out")):
-	print("md5 of the current release does not exist, creating")
-	curhash = md5(subprocess.check_output(["/home/e/chromereleasenotifier/target/release/chromereleasesnotifier", "print"])).hexdigest()
+	print("output of the current release does not exist, creating")
+	curhash = subprocess.check_output(["/home/e/chromereleasenotifier/target/release/chromereleasesnotifier", "print"]).decode('utf8');
 	with open(f"{SCRDIR}/prevoutput.out", "w") as file:
 		file.write(curhash)
 		file.close()
@@ -29,11 +29,12 @@ async def fetchreleases(ctx):
 @tasks.loop(minutes=5)
 async def timedfetch():
 	with open(f"{SCRDIR}/prevoutput.out", "r") as chashfile:
-		curhash = md5(subprocess.check_output(["/home/e/chromereleasenotifier/target/release/chromereleasesnotifier", "print"])).hexdigest()
-		if chashfile.read() != curhash:
+		curhash = subprocess.check_output(["/home/e/chromereleasenotifier/target/release/chromereleasesnotifier", "print"]).decode('utf8')
+		oldhash = chashfile.read();
+		if oldhash != curhash:
 			print("chrome release detected!")
 			channel = bot.get_channel(channelId)
-			await channel.send("<@&1134322964448432138> konqi has delivered", embed=createEmbed())
+			await channel.send("<@&1134322964448432138> konqi has delivered", embed=createEmbed(curhash, oldhash))
 			print("sent timed message")
 			chashfile.close()
 			with open(f"{SCRDIR}/prevoutput.out", "w") as chashfilew:
@@ -49,19 +50,23 @@ async def timedfetch():
 async def beforetimedfetch():
 	await bot.wait_until_ready()
 
-def createEmbed():
-	crnprint = subprocess.check_output(["/home/e/chromereleasenotifier/target/release/chromereleasesnotifier", "print"]).decode('utf8')
+def createEmbed(curout,oldout):
+	if not curout:
+	    crnprint = subprocess.check_output(["/home/e/chromereleasenotifier/target/release/chromereleasesnotifier", "print"]).decode('utf8')
+	else: 
+	    crnprint = curout
 	crnprint = crnprint.split(sep="__CUT_HERE")
-
-	crnprint.pop()
+	if oldout:
+	    oldoutsplit = oldout.split(sep="__CUT_HERE")
+	    crnprint = [x for x in crnprint if x not in oldoutsplit] 
 
 	embed=nextcord.Embed(title="Chrome Releases Notifier", description="Releases fetched from Google", url="https://chromereleases.googleblog.com/")
 	embed.set_author(name=random.choice(people), url="https://mercurywork.shop", icon_url="https://cdn.discordapp.com/attachments/1040039623323299992/1095547450019827773/0755fbdafab457bc.png")
 	embed.set_thumbnail(url="https://media.discordapp.net/attachments/1040039623323299992/1095547290845970462/250px-Mascot_konqi.png")
-	embed.set_footer(text="Chrome Releases Notifier")
+	embed.set_footer(text="Delivered proudly by Konqi :3")
 
 	for p in crnprint:
-		embed.add_field(name="Chrome Release", value=p, inline=False)
+		embed.add_field(name=("New Chrome Release" if oldout else "Chrome Release"), value=p, inline=False)
 	return embed
 
 timedfetch.start()
